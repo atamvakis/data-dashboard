909aee590a0ada095f9a63011b6786f362a56d20
atamvakis <tamvakis@live.unc.edu>
2020-11-02 22:16:28 -0500

    Final Update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
+//Variables for Line Chart
+var month = [];
+var gender = [];
 var numOfDeaths = [];
-var ageGroup = [];
+
+//Variables for Pie Chart One
+var deathsByRace = [];
+var race = [];
+
+
 $(document).ready( function () {
     console.log("Hello");
     $('#dataTable').DataTable();
@@ -7,31 +15,42 @@ $(document).ready( function () {
 });
 
 function loadJSON() {
-  $.getJSON("../data/deaths.json", function (deaths){
-    console.log(deaths);
+  /*$.getJSON("../data/deaths.json", function (deaths){
     parseData(deaths);
+  });*/
+
+  $.getJSON("../data/line.json", function (line){
+    //console.log(line);
+    parseData(line);
+  });
+
+  $.getJSON("../data/race.json", function (race){
+    //console.log(race);
+    parseData(race);
+  });
+
+  $.getJSON("../data/age.json", function (age){
+    //console.log(age);
+    parseData(age);
   });
 }
 
-function parseData(deaths){
-
-  var html = "";
-
-  $.each(deaths, function(index){
-    console.log(deaths[index].NumOfDeaths);
-    html += '<p>' + deaths[index].numofdeaths + '</p>';
-    html += '<ul><li>Age Group' + deaths[index].agegroup + '</li>';
-    html += '<li>Sex' + deaths[index].sex + '</li></ul>';
-
-    numOfDeaths.push(parseFloat(deaths[index].numofdeaths));
-    ageGroup.push(parseFloat(deaths[index].agegroup));
+function parseData(line){
+  console.log('i am here');
+  $.each(line, function(index){
+      month.push(parseFloat(line[index].Month));
+      gender.push(parseFloat(line[index].Gender));
+      numOfDeaths.push(parseFloat(line[index].NumOfDeaths));
   });
-  numOfDeaths.unshift("Number Of Deaths");
-  ageGroup.unshift("Age Group");
-  $("#test").append(html);
-}
 
+  console.log(line);
+  month.unshift("Month");
+  gender.unshift("Gender");
+  numOfDeaths.unshift("Number of Deaths");
+
+//I was unable to make this chart function with JSON data
 //Line Chart
+/*
 var chart = c3.generate({
     bindto: '#chart',
     data: {
@@ -54,16 +73,86 @@ var chart = c3.generate({
     color: {
         pattern: ['#000080', '#1f77b4', '#aec7e8', ]
     }
-});
+}); */
+
+}
+
+function parseData(race){
+
+  //$.each(race, function(index){
+      //deathsByRace.push(parseFloat(race[index].NumOfDeaths));
+      //race.push(parseFloat(race[index].race));
+      race.forEach(function(e) {
+        race.push(e.Race);
+        deathsByRace[e.Race] = e.NumOfDeaths;
+  });
+
+    deathsByRace.unshift("Number of Deaths");
+    race.unshift("Race/Ethnicity");
+
+}
+
 
 //Pie Chart for Race
+//I was unable to populate with JSON data
+  /*var pieChartOne = c3.generate({
+      bindto: '#pie-chart-one',
+      data: {
+        json: [ deathsByRace ],
+        keys: {
+            value: [ race ]
+        },
+          type : 'pie',
+          onclick: function (d, i) { console.log("onclick", d, i); },
+          onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+          onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+      },
+      color: {
+          pattern: ['#50BBE6', '#0BA4E0', '#097FAD', '#224F61','#054761', '#000080']
+      }
+  });
+
+}*/
+
+
+
+//Line Chart, no JSON data
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      x: 'x',
+      columns: [
+        ['x', '5', '6', '7', '8'],
+        ['Total', 37940, 17718, 30401, 28352],
+        ['Males', 19220, 9248, 17004, 15459],
+        ['Females', 18720, 8470, 13397, 12893]
+      ]
+    },
+    axis: {
+      y: {
+        label: { // ADD
+          text: 'Number of Cases',
+          position: 'outer-middle'
+        }
+      }
+    },
+    color: {
+          pattern: ['#2234A8', '#4467C4','#659BDF']
+    }
+});
+
+
+//Pie Chart for Race, no JSON data
 var pieChartOne = c3.generate({
     bindto: '#pie-chart-one',
     data: {
-        // iris data from R
         columns: [
-            ['data1', 30],
-            ['data2', 120],
+            ['Hispanic', 27682],
+            ['American Indian/Alaska Native', 1533],
+            ['Asian', 3964],
+            ['Black', 21423],
+            ['White', 58746],
+            ['Other', 1063],
         ],
         type : 'pie',
         onclick: function (d, i) { console.log("onclick", d, i); },
@@ -71,18 +160,22 @@ var pieChartOne = c3.generate({
         onmouseout: function (d, i) { console.log("onmouseout", d, i); }
     },
     color: {
-        pattern: ['#1f77b4', '#aec7e8']
+          pattern: ['#191970', '#00008C', '#2234A8', '#4467C4','#659BDF', '#87CEFB']
     }
 });
 
-//Pie Chart for Age
+//Pie Chart for US Demographics, No JSON (data from Census Bureau)
 var pieChartTwo = c3.generate({
     bindto: '#pie-chart-two',
     data: {
         // iris data from R
         columns: [
-            ['data1', 30],
-            ['data2', 120],
+          ['Hispanic', 18.5],
+          ['American Indian/Alaska Native', 1.3],
+          ['Asian', 5.9],
+          ['Black', 13.4],
+          ['White', 60.1],
+          ['Other', 0.8],
         ],
         type : 'pie',
         onclick: function (d, i) { console.log("onclick", d, i); },
@@ -90,9 +183,59 @@ var pieChartTwo = c3.generate({
         onmouseout: function (d, i) { console.log("onmouseout", d, i); }
     },
     color: {
-        pattern: ['#1f77b4', '#aec7e8']
+          pattern: ['#191970', '#00008C', '#2234A8', '#4467C4','#659BDF', '#87CEFB']
     }
 });
+
+//Bar Chart for
+var barChart = c3.generate({
+    bindto: '#bar-chart',
+    data: {
+      x : 'x',
+      columns: [
+          ['x', 'Northeast', 'South', 'Midwest', 'West'],
+          ['Number of Deaths', 23472, 52258, 17724, 20957],
+        ],
+        //groups: [
+          //['download', 'loading']
+        //],
+        type: 'bar'
+      },
+      axis: {
+        x: {
+          type: 'category' // this needed to load string x value
+        }
+      },
+      color: {
+            pattern: ['#191970', '#00008C', '#2234A8', '#4467C4','#659BDF', '#87CEFB']
+      }
+    });
+
+var chart = c3.generate({
+  bindto: '#age',
+    data: {
+        columns: [
+          ['Under 1 Year', 14],
+          ['1 to 17', 59],
+          ['18 to 39', 2113],
+          ['40 to 64', 22788],
+          ['65 to 84', 54647],
+          ['85 + ', 34788],
+        ],
+        type : 'donut',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    },
+    color: {
+        pattern: ['#191970', '#00008C', '#2234A8', '#4467C4','#659BDF', '#87CEFB']
+    },
+    donut: {
+        title: ""
+    }
+});
+
+
 
 
 $('#dataTable').DataTable( {
@@ -107,4 +250,4 @@ $('#dataTable').DataTable( {
     {"data" : "PlaceofDeath"}
   ]
 
-} );
+});
